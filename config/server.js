c6f410b87b18b8c064612391222cc76a50e01112
Breno Sapucaia <brenoss.magalhaes@outlook.com>
2021-08-13 11:04:52 -0300

    chore: init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '04808afb6649f57f939263b2f208004d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '921c3f87-8666-48d9-811c-192c5819ebeb'
+};
